aa71632472c673bb80d390892b0a50ef489957a2
Arshdeep Singh <arsh@Arshdeeps-MacBook-Pro.local>
2018-08-06 03:25:08 -0400

    updated

---
+++ assets/js/main.js
@@ -0,0 +1,54 @@
+jQuery(document).ready(function($) {
+
+
+    /*======= Skillset *=======*/
+
+    $('.level-bar-inner').css('width', '0');
+
+    $(window).on('load', function() {
+
+        $('.level-bar-inner').each(function() {
+
+            var itemWidth = $(this).data('level');
+
+            $(this).animate({
+                width: itemWidth
+            }, 800);
+
+        });
+
+    });
+
+    /* Bootstrap Tooltip for Skillset */
+    $('.level-label').tooltip();
+
+
+    /* jQuery RSS - https://github.com/sdepold/jquery-rss */
+
+
+
+    /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
+    GitHubCalendar("#github-graph", "Arsh-Boparai");
+    var modal = document.getElementById('myModal');
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var img = document.getElementById('myImg');
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+    img.onclick = function() {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+
+
+});
